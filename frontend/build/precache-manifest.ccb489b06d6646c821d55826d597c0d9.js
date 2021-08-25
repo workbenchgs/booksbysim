31077523e7edd93ec4188e58ebf7910a22241f4e
workbenchgs <80515795+workbenchgs@users.noreply.github.com>
2021-08-25 08:46:26 +0530

    first commit

---
+++ frontend/build/precache-manifest.ccb489b06d6646c821d55826d597c0d9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa54ff8049d70fe89adeb4b666a0e24b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6db7479f3260a2f2c21",
+    "url": "/static/css/2.3fc42fd8.chunk.css"
+  },
+  {
+    "revision": "a965a0bb7f0dacf7d248",
+    "url": "/static/css/main.3d4b2425.chunk.css"
+  },
+  {
+    "revision": "b6db7479f3260a2f2c21",
+    "url": "/static/js/2.c8e8415e.chunk.js"
+  },
+  {
+    "revision": "c87e50d81cc7b5311525cc6fd5482ea5",
+    "url": "/static/js/2.c8e8415e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a965a0bb7f0dacf7d248",
+    "url": "/static/js/main.8357b63b.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
